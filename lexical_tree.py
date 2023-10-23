6c7239954fe36dfccdf24b22a6643294e89a3074
medalha01 <hiperboss2009@gmail.com>
2023-10-22 21:57:51 -0300

    Inicio do fim

--- lexical_tree.py
+++ lexical_tree.py
@@ -1,9 +1,14 @@
 class LexicalTree:
     def __init__(self):
         self.nodes = []
+        self.follow_pos = dict()
 
     def add_node(self, node):
         self.nodes.append(node)
 
     def get_nodes(self):
         return self.nodes
+
+    def calculate_follow_pos(self):
+        for node in self.nodes:
+            node.calculate_follow_pos()
